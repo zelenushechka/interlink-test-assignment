f2397ae6fb2ba57f06ebb576c6ee581c6fe64127
zelenushechka <zelikangelina@gmail.com>
2022-03-24 20:52:37 +0100

    Database models

--- my_project/app/models.py
+++ my_project/app/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+
+class PortComponent(models.Model):
+    location = models.CharField(max_length=30, null=False, blank=False)
+
+    def __str__(self):
+        return self.location
+
+
+class User(models.Model):
+    port = models.ForeignKey(PortComponent, on_delete=models.CASCADE)
+    name = models.CharField(max_length=30, null=False, blank=False)
+
+    def __str__(self):
+        return self.name
