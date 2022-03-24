78bad1a9fa5c8e38143a0db6fff48ac5a86d8535
zelenushechka <zelikangelina@gmail.com>
2022-03-24 21:07:13 +0100

    Add admin and view

--- my_project/app/admin.py
+++ my_project/app/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
 
-# Register your models here.
+from my_project.app.models import User, PortComponent
+
+
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(PortComponent)
+class PortComponentAdmin(admin.ModelAdmin):
+    pass
--- my_project/app/views.py
+++ my_project/app/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
+from django.http import HttpResponse
+from my_project.app.models import PortComponent
 
-# Create your views here.
+
+def index(request):
+    ports = PortComponent.objects.all()
+    response = []
+    for port in ports:
+        users_text = ', '.join(user.name for user in port.user_set.all())
+        response.append(f'<strong>{port.location}</strong>: {users_text or "-"}')
+
+    return HttpResponse('<br>'.join(response))
